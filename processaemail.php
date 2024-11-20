7a30fba4f1479fb8f159144ce8f1fa582cc4265c
Oseias <soseias894@gmail.com>
2024-11-19 23:25:49 -0300

    AGORA VAI

--- processaemail.php
+++ processaemail.php
@@ -5,13 +5,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
 
     // Configurações de conexão com o banco de dados
-$host = 'sql105.infinityfree.com'; // Host do seu banco de dados
-$dbname = 'if0_37745435_cadastro'; // Nome do banco de dados
-$username = 'if0_37745435'; // Usuário do banco
-$password = 'KMrIBprDe4'; // Senha do banco
+    $host = 'sql105.infinityfree.com';  // Host do seu banco de dados
+    $dbname = 'if0_37745435_cadastro'; // Nome do banco de dados
+    $username = 'if0_37745435';        // Usuário do banco
+    $password = 'KMrIBprDe4';          // Senha do banco
 
     // Conexão com o banco de dados MySQL
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($host, $username, $password, $dbname);
 
     // Verifica se a conexão foi bem-sucedida
     if ($conn->connect_error) {
@@ -23,7 +23,7 @@ $password = 'KMrIBprDe4'; // Senha do banco
 
     // Preparar a declaração
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ss", $nome, $email);  // "ss" indica que são dois parâmetros do tipo string
+    $stmt->bind_param("s", $email);  // "s" indica que é um parâmetro do tipo string
 
     // Executar a consulta
     if ($stmt->execute()) {
